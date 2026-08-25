245a0afe579cb143d391a859cc752b50e4d53613
Sourabh <karbburn@gmail.com>
2026-08-25 19:56:08 +0530

    updated portfolio url

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value:
+              "frame-ancestors 'self' https://sourabhpradhan.in https://www.sourabhpradhan.in http://localhost:* https://*.vercel.app;",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
